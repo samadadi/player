1c042a17143aef4c3009ee6c9388540ac8a946a2
Saeed Madadi <com2434@gmail.com>
2023-03-15 23:28:47 +0330

    update run.sh

--- Run.sh
+++ Run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-flatpak-builder --user --install --ccache --force-clean --state-dir state-dir build-dir com.samadadi.Player.json && flatpak run com.samadadi.Player
+flatpak-builder --ccache --force-clean --state-dir state-dir build-dir com.samadadi.Player.json
